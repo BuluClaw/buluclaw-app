d063b572f5d36773a4f107d7764c25394a9ea311
Sachin Gill <sgill4824@gmail.com>
2026-02-25 17:53:49 +0530

    fix: correct next.js standalone build config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
+
   experimental: {
+    turbo: {},       
     serverActions: {
-      allowedOrigins: ['*'],
+      allowedOrigins: ["*"],
     },
   },
 };
