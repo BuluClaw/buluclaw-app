ed71c2ac36363e3c55971f71f104139a852a704d
Sachin Gill <sgill4824@gmail.com>
2026-02-25 17:41:25 +0530

    Fix:next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone"
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*'],
+    },
+  },
 };
 
 module.exports = nextConfig;
