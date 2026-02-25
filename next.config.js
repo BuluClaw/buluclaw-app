aa9b7072347f0a79d1e9ae4814b2123fe27ffb5a
Sachin Gill <sgill4824@gmail.com>
2026-02-25 13:49:40 +0530

    Fixed build export issue for Railway

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone"
+  output: "standalone",
+  experimental: {
+    turbo: true
+  }
 };
 
 module.exports = nextConfig;
