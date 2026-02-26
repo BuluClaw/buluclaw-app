23e56bc58e252e9d92506de2e23a6583bd175cc2
Sachin Gill <sgill4824@gmail.com>
2026-02-26 16:06:10 +0530

    git add . git commit -m "fix: standalone server final" git push

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["*"],
-    },
-  },
 };
 
 module.exports = nextConfig;
