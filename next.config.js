5dca8323bbe98a9d0f4839249429974cc29fb8e4
Sachin Gill <sgill4824@gmail.com>
2026-02-26 10:56:32 +0530

    fix: static assets load

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  assetPrefix: '',
+  basePath: '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
