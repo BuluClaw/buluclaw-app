d8691d3f67e94d3fd4c86d469c4a68434df53d28
Sachin Gill <sgill4824@gmail.com>
2026-03-07 17:31:49 +0530

    fix razorpay subscription button

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -142,7 +142,7 @@ src="https://cdn.razorpay.com/static/widget/subscription-button.js"
 data-subscription_button_id="pl_SNUN6sr0ET9inS"
 data-button_theme="brand-color"
 
-data-success_url="https://www.buluclaw.com/payment-success"
+data-success_url="https://www.buluclaw.com/dashboard"
 data-cancel_url="https://www.buluclaw.com/checkout"
 
 ></script>
