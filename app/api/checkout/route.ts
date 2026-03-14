e13865733690780a1c6f4041d2fe77b14ffcd086
Sachin Gill <sgill4824@gmail.com>
2026-03-14 11:45:25 +0530

    polar checkout setup

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,6 +1,21 @@
-import { Checkout } from "@polar-sh/nextjs";
+import { NextResponse } from "next/server"
 
-export const GET = Checkout({
-  accessToken: process.env.POLAR_ACCESS_TOKEN!,
-  successUrl: process.env.POLAR_SUCCESS_URL!
-});
+export async function GET() {
+
+const res = await fetch("https://api.polar.sh/v1/checkouts", {
+method: "POST",
+headers: {
+Authorization: `Bearer ${process.env.POLAR_ACCESS_TOKEN}`,
+"Content-Type": "application/json",
+},
+body: JSON.stringify({
+product_id: "ed144c7c-d2ca-4b88-8767-20a27c54e53f",
+success_url: process.env.POLAR_SUCCESS_URL,
+})
+})
+
+const data = await res.json()
+
+return NextResponse.redirect(data.url)
+
+}
