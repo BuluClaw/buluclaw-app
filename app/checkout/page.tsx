a8d4dae3d1f19696eccf663935696b7fa1bf8723
Sachin Gill <sgill4824@gmail.com>
2026-03-22 10:17:53 +0530

    Fix:checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-export default function CheckoutPage() {
-  return (
-    <div className="h-screen w-screen bg-black">
-      
-      <iframe
-        src="https://buy.polar.sh/polar_cl_yLgMY3vkBYbBBnGsVQZH7urSahmg2GGfry12n8cRC"
-        className="w-full h-full border-0"
-        allow="payment"
-      />
+import { useEffect } from "react";
 
+export default function CheckoutPage() {
+
+  useEffect(() => {
+    window.location.href =
+      "https://buy.polar.sh/polar_cl_yLgMY3vkBYbBBnGsVQZH7urSahmg2GGfry12n8cRC";
+  }, []);
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white">
+      Redirecting to secure checkout...
     </div>
   );
 }
