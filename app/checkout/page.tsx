3a2699c9aa557d58f6040bbcb2e2814904f921d0
Sachin Gill <sgill4824@gmail.com>
2026-03-22 10:56:32 +0530

    Fix checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -6,12 +6,12 @@ export default function CheckoutPage() {
 
   useEffect(() => {
     window.location.href =
-      "https://buy.polar.sh/polar_cl_yLgMY3vkBYbBBnGsVQZH7urSahmg2GGfry12n8cRC";
+      "https://buy.polar.sh/polar_cl_yLgMYY3vkBYybBBnGsVQZH7urSahmg2GGfryl2n8cRC";
   }, []);
 
   return (
-    <div className="h-screen flex items-center justify-center bg-black text-white">
-      Redirecting to secure checkout...
+    <div className="h-screen flex items-center justify-center">
+      Redirecting...
     </div>
   );
 }
