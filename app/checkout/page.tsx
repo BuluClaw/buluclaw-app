715b7a0423f8f7cd26a88b4d66cfd0274c83f98b
Sachin Gill <sgill4824@gmail.com>
2026-03-22 11:24:57 +0530

    Fix checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -5,9 +5,18 @@ import { useEffect } from "react";
 export default function CheckoutPage() {
 
   useEffect(() => {
+
+    // google login se email milti hai
+    const email = localStorage.getItem("user_email");
+
     window.location.href =
-      "https://buy.polar.sh/polar_cl_yLgMYY3vkBYybBBnGsVQZH7urSahmg2GGfryl2n8cRC";
+      `https://buy.polar.sh/polar_cl_yLgMYY3vkBYybBBnGsVQZH7urSahmg2GGfryl2n8cRC?email=${email}`;
+
   }, []);
 
-  
+  return (
+    <div className="h-screen flex items-center justify-center">
+      Redirecting to secure checkout...
+    </div>
+  );
 }
