805a39cb9b758f2abee2de1a36bef182e7410c4d
Sachin Gill <sgill4824@gmail.com>
2026-03-22 12:06:46 +0530

    Fix: checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -5,14 +5,18 @@ import { useEffect } from "react";
 export default function CheckoutPage() {
 
   useEffect(() => {
+    
 
     const email =
       localStorage.getItem("user_email");
-
+      
     window.location.href =
-      `https://buy.polar.sh/polar_c_lTh19K9J4McsuNUUQsHw00r21gfTQy5KAkugY1u6Kdo?email=${email}`;
-
+      "https://buy.polar.sh/polar_cl_yLgMYY3vkBYybBBnGsVQZH7urSahmg2GGfryl2n8cRC";
   }, []);
 
-  return null;
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white">
+      Redirecting to secure checkout...
+    </div>
+  );
 }
