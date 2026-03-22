72cc98573721770f3cf6de6a246342571c9fd3b1
Sachin Gill <sgill4824@gmail.com>
2026-03-22 10:04:29 +0530

    fix:checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -2,15 +2,19 @@
 
 export default function CheckoutPage() {
   return (
-    <div className="min-h-screen bg-[#070707] flex items-center justify-center">
-
-      <div className="w-full max-w-md">
-
-        <div className="bg-black border border-neutral-800 rounded-2xl p-4 shadow-2xl">
-
+    <div className="min-h-screen bg-[#070707] flex justify-center pt-16">
+      
+      <div className="w-[420px] max-w-[95%]">
+        
+        <div className="bg-black border border-neutral-800 rounded-2xl p-3 shadow-2xl">
+          
           <iframe
-            src="https://buy.polar.sh/polar_cl_yLgMYY3vkBYybBBnGsVQZH7urSahmg2GGfryl2n8cRC"
-            className="w-full h-[720px] rounded-xl"
+            src="https://buy.polar.sh/polar_cl_yLgMY3vkBYbBBnGsVQZH7urSahmg2GGfry12n8cRC"
+            className="w-full rounded-xl"
+            style={{
+              height: "760px",
+              border: "none"
+            }}
             allow="payment"
           />
 
