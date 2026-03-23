6609ce18e6a019bf846386d6ecb1cdc010171f18
Sachin Gill <sgill4824@gmail.com>
2026-03-23 17:00:03 +0530

    Fix:check payment

--- app/api/check-payment/route.ts
+++ app/api/check-payment/route.ts
@@ -6,16 +6,8 @@ const url = new URL(req.url)
 
 const paid = url.searchParams.get("paid")
 
-if(paid==="true"){
-
 return NextResponse.json({
-paid:true
-})
-
-}
-
-return NextResponse.json({
-paid:false
+paid: paid==="true"
 })
 
 }
