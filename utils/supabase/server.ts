83933f5d51bad885bf3e95f50371e97470aaef8f
Sachin Gill <sgill4824@gmail.com>
2026-03-24 22:13:20 +0530

    git add . git commit -m "fix supabase session" git push

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,21 +1,34 @@
 import { createServerClient } from "@supabase/ssr"
 import { cookies } from "next/headers"
 
-export async function createClient() {
+export async function createClient(){
 
   const cookieStore = await cookies()
 
   return createServerClient(
+
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+
     {
+
       cookies: {
-        get(name: string) {
+
+        get(name: string){
+
           return cookieStore.get(name)?.value
+
         },
-        set() {},
-        remove() {}
+
+        set(){},
+
+        remove(){}
+
       }
+
     }
+
   )
+
 }
