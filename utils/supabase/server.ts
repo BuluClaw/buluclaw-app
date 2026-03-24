898ab86703adbf56451e4f5bb95695850a3c9f03
Sachin Gill <sgill4824@gmail.com>
2026-03-24 16:16:47 +0530

    git add . git commit -m "fix cookies await" git push

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -6,24 +6,16 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-
     {
-
       cookies: {
-
         get(name: string) {
-
           return cookieStore.get(name)?.value
-
         },
-
-      },
-
+        set() {},
+        remove() {}
+      }
     }
-
   )
-
 }
