049d9e6bb220c4dff259328de0df5101e1c266a0
Sachin Gill <sgill4824@gmail.com>
2026-03-25 13:37:34 +0530

    Fix:error

--- app/page.tsx
+++ app/page.tsx
@@ -33,14 +33,6 @@ useEffect(() => {
 
   const [token, setToken] = useState("");
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://checkout.razorpay.com/v1/checkout.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-
-
 
 const connectTelegram = async () => {
 
